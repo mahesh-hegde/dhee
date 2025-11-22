09a4edab9acf4e5f3f701532aa0f893830e34b9e
Mahesh Hegde <46179734+mahesh-hegde@users.noreply.github.com>
2025-11-22 19:45:43 +0530

    feat: Add REGEXP function to modernc.org/sqlite driver

--- app/docstore/sqlite_driver_modernc.go
+++ app/docstore/sqlite_driver_modernc.go
@@ -4,7 +4,60 @@
 package docstore
 
 import (
-	_ "modernc.org/sqlite"
+	"database/sql/driver"
+	"errors"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/patrickmn/go-cache"
+	sqlite "modernc.org/sqlite"
 )
+
+var regexCache = cache.New(2*time.Minute, 5*time.Minute)
+
+func init() {
+	sqlite.MustRegisterDeterministicScalarFunction(
+		"regexp",
+		2,
+		func(ctx *sqlite.FunctionContext, args []driver.Value) (driver.Value, error) {
+			var s1 string
+			var s2 string
+
+			switch arg0 := args[0].(type) {
+			case string:
+				s1 = arg0
+			default:
+				return nil, errors.New("expected argv[0] to be text")
+			}
+
+			switch arg1 := args[1].(type) {
+			case string:
+				s2 = arg1
+			default:
+				return nil, errors.New("expected argv[1] to be text")
+			}
+
+			re := s1
+			if !strings.HasPrefix(s1, "(?s)") {
+				re = "(?s)" + s1
+			}
+
+			var compiledRe *regexp.Regexp
+			var err error
+
+			if reFromCache, found := regexCache.Get(re); found {
+				compiledRe = reFromCache.(*regexp.Regexp)
+			} else {
+				compiledRe, err = regexp.Compile(re)
+				if err != nil {
+					return false, err
+				}
+				regexCache.Set(re, compiledRe, cache.DefaultExpiration)
+			}
+			return compiledRe.MatchString(s2), nil
+		},
+	)
+}
 
 const SQLiteDriverName = "sqlite"
